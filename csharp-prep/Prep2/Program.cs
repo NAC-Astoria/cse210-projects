1d45938e41dc7506e5afb129c782086ea2f20b9e
Astoria Carroll <car20132@byui.edu>
2026-09-19 12:07:21 -0600

    Completed Prep2 activity.

--- csharp-prep/Prep2/Program.cs
+++ csharp-prep/Prep2/Program.cs
@@ -4,6 +4,44 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep2 World!");
+        string letter = "";
+
+        Console.Write("What is your grade percentage? ");
+        string userGrade = Console.ReadLine();
+        int grade = int.Parse(userGrade);
+
+        if (grade >= 90)
+        {
+            letter = "A";
+        }
+        else if (grade >= 80)
+        {
+            letter = "B";        
+        }
+        else if (grade >= 70)
+        {
+            letter = "C";        
+        }
+        else if (grade >= 60)
+        {
+            letter = "D";        
+        }
+        else
+        {
+            letter = "F";        
+        }
+
+        Console.WriteLine($"You have a {letter}");
+
+
+        if (grade >= 70)
+        {
+            Console.WriteLine("Great job! You are passing the class.");
+        }
+        else
+        {
+            Console.WriteLine("Sorry, you are not passing the class. Better luck next time.");
+        }
+
     }
 }
