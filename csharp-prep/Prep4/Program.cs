93c62f9d23e6802a2671a3b5093ed96d563c49a0
Astoria Carroll <car20132@byui.edu>
2026-09-24 12:09:05 -0600

    Completed Prep 4

--- csharp-prep/Prep4/Program.cs
+++ csharp-prep/Prep4/Program.cs
@@ -1,9 +1,66 @@
 using System;
+using System.Collections.Generic;
 
 class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep4 World!");
+        List<int> numbers = new List<int>();
+        int newNumber = -1;
+
+        Console.WriteLine("Enter a list of numbers, type 0 when finished.");
+
+        // Create number list from user input
+        while (newNumber != 0)
+        {
+            Console.Write("Enter a number ");
+            newNumber = int.Parse(Console.ReadLine());
+            if (newNumber != 0)
+            {
+                numbers.Add(newNumber);
+            }
+        }
+
+        // Calculate sum
+        int total = 0;
+        foreach (int number in numbers)
+        {
+            total += number;
+        }
+        Console.WriteLine($"The sum is: {total}");
+
+        // Calculate average
+        float average = ((float)total) / numbers.Count;
+        Console.WriteLine($"The average is: {average}");
+
+        // Determine largest
+        int largestNumber = 0;
+        foreach (int number in numbers)
+        {
+            if (number > largestNumber)
+            {
+                largestNumber = number;
+            }  
+        }
+        Console.WriteLine($"The largest number is: {largestNumber}");
+
+        // Determine smallest positive number
+        int smallestNumber = 999;
+        foreach (int number in numbers)
+        {
+            if (number < smallestNumber && number > 0)
+            {
+                smallestNumber = number;
+            }
+        }
+        Console.WriteLine($"The smallest positive number is {smallestNumber}");
+
+        // Sort list
+        numbers.Sort();
+        foreach (int number in numbers)
+        {
+            Console.WriteLine(number);
+        }
+
     }
 }
