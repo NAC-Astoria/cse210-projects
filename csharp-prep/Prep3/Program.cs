e03d4679b5ca2aacb1382b8c7f7e85921ae67c5a
Astoria Carroll <car20132@byui.edu>
2026-09-24 10:46:09 -0600

    Completed Prep 3

--- csharp-prep/Prep3/Program.cs
+++ csharp-prep/Prep3/Program.cs
@@ -4,6 +4,38 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep3 World!");
+        string playAgain;
+        int userGuess = 0;
+
+        do
+        {
+            Random randomGenerator = new Random();
+            int magicNumber = randomGenerator.Next(1,101);
+
+            userGuess = -1;
+            int guessCount = 0;
+            while(userGuess != magicNumber)
+            {
+                guessCount++;
+                Console.Write("Please guess the magic number: ");
+                userGuess = int.Parse(Console.ReadLine());
+
+                if (userGuess == magicNumber)
+                {
+                    Console.WriteLine("You guessed it!");
+                    Console.WriteLine($"It took you {guessCount} guesses.");
+                }
+                else if (userGuess > magicNumber)
+                {
+                    Console.WriteLine("Lower");
+                }
+                else if (userGuess < magicNumber)
+                {
+                    Console.WriteLine("Higher");
+                }
+            }
+            Console.Write("Would you like to play again? ");
+            playAgain = Console.ReadLine();
+        } while (playAgain == "yes");
     }
 }
